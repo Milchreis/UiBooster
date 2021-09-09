4fe2cdfbc97a5ca4ccc6b5f5ee725bd7bad73e90
milchreis <nick-m@gmx.de>
2021-09-09 20:08:08 +0200

    Revert autoformatting

--- src/test/java/de/milchreis/uibooster/FormBuilderTest.java
+++ src/test/java/de/milchreis/uibooster/FormBuilderTest.java
@@ -157,8 +157,7 @@ class FormBuilderTest {
     public void test_disable_button_dialog() throws InterruptedException {
         Form form = booster
                 .createForm("Test")
-                .addButton("Prev", () -> {
-                }).setDisabled().setID("test")
+                .addButton("Prev", () -> {}).setDisabled().setID("test")
                 .run();
 
         sleep(1000);
@@ -170,12 +169,9 @@ class FormBuilderTest {
         Form form = booster
                 .createForm("Test")
                 .startRow()
-                .addButton("On", () -> {
-                }).setID("on1")
-                .addButton("Off", () -> {
-                }).setDisabled().setID("off1")
-                .addButton("On", () -> {
-                }).setID("on2")
+                .addButton("On", () -> {}).setID("on1")
+                .addButton("Off", () -> {}).setDisabled().setID("off1")
+                .addButton("On", () -> {}).setID("on2")
                 .endRow()
                 .show();
 
