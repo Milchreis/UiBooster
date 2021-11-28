d9cbe1f2555ce41ed79dbd796eb604bc6477895a
milchreis <nick-m@gmx.de>
2021-11-28 17:14:36 +0100

    Bugfix (#58)

--- src/main/java/de/milchreis/uibooster/components/ExceptionDialog.java
+++ src/main/java/de/milchreis/uibooster/components/ExceptionDialog.java
@@ -11,22 +11,15 @@ import java.io.StringWriter;
 public class ExceptionDialog {
 
     public static void showDialog(String message, String title, UiBoosterOptions options, Exception exception) {
-        JPanel panel = createExceptionPanel(message, exception);
+        JPanel panel = createExceptionPanel(exception);
 
         SimpleBlockingDialog dialog = new SimpleBlockingDialog(panel);
         dialog.showDialog(message, title, options.getIconPath());
     }
 
-    private static JPanel createExceptionPanel(String message, Exception exception) {
+    private static JPanel createExceptionPanel(Exception exception) {
         JPanel panel = new JPanel(new BorderLayout());
-        panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-
-        JLabel messageLabel = new JLabel(message);
-        messageLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        messageLabel.setHorizontalAlignment(JLabel.CENTER);
-
-        JPanel textPanel = new JPanel(new BorderLayout());
-        textPanel.add(messageLabel, BorderLayout.NORTH);
+        panel.setBorder(new EmptyBorder(10, 0, 0, 0));
 
         StringWriter writer = new StringWriter();
         PrintWriter printWriter = new PrintWriter(writer);
