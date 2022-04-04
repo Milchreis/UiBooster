407e445fbc74ca5edf11e514555f4a79f9ba8a85
milchreis <nick-m@gmx.de>
2022-04-04 18:12:05 +0200

    Formatting

--- src/main/java/de/milchreis/uibooster/model/options/InferredUiBoosterOptions.java
+++ src/main/java/de/milchreis/uibooster/model/options/InferredUiBoosterOptions.java
@@ -3,23 +3,23 @@ package de.milchreis.uibooster.model.options;
 import com.formdev.flatlaf.FlatDarculaLaf;
 import de.milchreis.uibooster.model.UiBoosterOptions;
 
-import javax.swing.LookAndFeel;
-import javax.swing.UIManager;
+import javax.swing.*;
 import javax.swing.plaf.basic.BasicLookAndFeel;
 
 /**
  * Uses/Infers an existing and already set look and feel for UIBooster.
  */
 public class InferredUiBoosterOptions extends UiBoosterOptions {
+
     public static BasicLookAndFeel InferredLookAndFeel() {
         try {
             LookAndFeel laf = UIManager.getLookAndFeel();
             if (laf == null) {
-                System.err.println("UIBooster: No previously set look anf feel found! Using default.");
+                System.err.println("UiBooster: No previously set look anf feel found! Using default.");
                 return new FlatDarculaLaf();
             }
             if (!(laf instanceof BasicLookAndFeel)) {
-                System.err.println("UIBooster: Look and feel is not an instance of BasicLookAndFeel! Using default.");
+                System.err.println("UiBooster: Look and feel is not an instance of BasicLookAndFeel! Using default.");
                 return new FlatDarculaLaf();
             }
             return (BasicLookAndFeel) laf;
